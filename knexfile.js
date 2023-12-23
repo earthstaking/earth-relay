289f24f80773669e7a6a8da615ff42d279ad1a08
Sukhveer Sanghera <github@everhusk.com>
2023-12-23 17:55:52 +0800

    Fix DATABASE_URL to DB_URI

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,12 @@
 module.exports = {
   client: 'pg',
-  connection: process.env.DATABASE_URI ? process.env.DATABASE_URI : {
+  connection: process.env.DB_URI ? process.env.DB_URI : {
     host: process.env.DB_HOST ?? 'localhost',
     port: process.env.DB_PORT ?? 5432,
     user: process.env.DB_USER ?? 'postgres',
     password: process.env.DB_PASSWORD ?? 'postgres',
     database: process.env.DB_NAME ?? 'nostream',
+    ssl: process.env.ssl ?? false
   },
   seeds: {
     directory: './seeds',
